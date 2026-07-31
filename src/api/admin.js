bc4c51e14843a9a12f123a89462fa6d313280467
Rajan1129 <shrmarajn70@gmail.com>
2026-07-31 09:50:58 +0530

    fix search

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,7 +1,14 @@
 // src/api/admin.js
 import axios from 'axios';
+import { API_URL } from "../config";
 
-const API_URL = 'http://localhost:5000/api';
+const api = axios.create({
+  baseURL: `${API_URL}/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
 
 const api = axios.create({
   baseURL: API_URL,
